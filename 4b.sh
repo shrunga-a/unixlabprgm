8a5fabe34ef4c704b2472a1874325bc8d1395ccc
Shrungashree c <73280862+shrunga-a@users.noreply.github.com>
2022-05-27 15:51:04 +0530

    Update 4b.sh

--- 4b.sh
+++ 4b.sh
@@ -1,18 +1,37 @@
-if [ $# -eq 3 ]
-then
-if [ -f $1 ]
-then
-l =` cat $1 | wc -l`
-if [ $2 -gt $3 -a $2 -gt $1 a $3 -gt $2 ]
-then
-head -n $1 $3 | tail -n +$2
-else
-echo "enter valid argument"
-fi
-else
-echo "file doesnt exist"
-fi
-else
-echo "entre the nunber of argument"
-fi
+#4b. Write a shell script that displays all the links to a file specified as the first argument to the
+#script. The second argument, which is optional, can be used to specify in which the search is to
+#begin. If this second argument is not present, the search is to begin in the current working
+#directory. In either case, the starting directory as well as its subdirectories at all levels must be
+#searched. The script need not include error checking.
 
+
+
+if [ $# -ge 1 ]
+then
+	if [ $# -eq 2 ]
+	then
+		sd=$2
+	else
+		sd="."
+	fi
+	
+	file=$1
+	if [ -f $file ]
+	then
+		
+		set -- `ls -i $file`
+		inode=$1
+		tfiles=`find $sd -inum $inode | wc -l`
+		if [ $tfiles -eq 1 ]
+		then
+			echo "$file has no link files"
+		else
+			find $sd -inum $inode
+			echo "The inode number for $tfiles files is $inode"
+		fi
+	else
+		echo "Give valid file"
+	fi
+else
+	echo "Give atleast one arguement"
+fi
