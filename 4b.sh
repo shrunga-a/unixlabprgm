f96bea11bc45e7c2aeb8e54934f4ec5f3a72a0fa
Shrungashree c <73280862+shrunga-a@users.noreply.github.com>
2022-06-01 12:42:56 +0530

    Update 4b.sh

--- 4b.sh
+++ 4b.sh
@@ -35,3 +35,28 @@ then
 else
 	echo "Give atleast one arguement"
 fi
+or
+
+file=$2
+if [ $# -eq 1 ]
+then
+	dirx="."
+else
+	if [ -e $2 ]
+	then
+		dirx="$2"
+	else
+		echo No such directory found
+	fi
+fi
+set -- `ls -l $file`
+lcnt=$2
+if [ $lcnt -eq 1 ]
+then
+	echo "No other links"
+	exit 0
+else
+	set -- `ls -i $file`
+	inode=$1
+	find "$dirx" -inum $inode -print
+fi
