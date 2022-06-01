3e5d0c56d1acd7c4a5a16e8d772566b0581acb19
Shrungashree c <73280862+shrunga-a@users.noreply.github.com>
2022-06-01 12:46:23 +0530

    Update 7a.sh

--- 7a.sh
+++ 7a.sh
@@ -1,20 +1,21 @@
 #Write a shell script that gets executed and displays the message either “Good Morning” or
 #“Good Afternoon” or “Good Evening” depending upon time at which the user logs in.
 
-set -- `who`
-u=$1
-time=$4
-set -- `echo $time | tr ":" " "`
-hr=$1
-if [ $hr -ge 5 ] && [ $hr -lt 12 ]
+user=`whoami`
+set -- `who | grep $user` 
+hr=$4
+echo User Login time $hr
+set -- `echo $hr | tr ":" " "`
+h=$1
+if [ $h -gt 4 ] && [ $h -lt 12 ]
 then
-	echo "Good morning($time) $u"
-elif [ $hr -ge 12 ] && [ $hr -lt 15 ]
+	echo Good morning Mr./Mrs. $user
+elif [ $h -gt 12 ] && [ $h -lt 16 ]
 then
-	echo "Good afternoon($time) $u"
-elif [ $hr -ge 15 ] && [ $hr -lt 19 ]
+	echo Good afternoon Mr./Mrs. $user
+elif [ $h -gt 16 ] && [ $h -lt 19 ]
 then
-	echo "Good evening($time) $u"
-else	
-	echo "Good night($time) $u"
+	echo Good evening Mr./Mrs. $user
+else
+	echo Good night Mr./Mrs. $user
 fi
